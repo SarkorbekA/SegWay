6821858aba24195d896c434d0f9fbdf09febc824
Sarkor2007 <106859987+Sarkor2007@users.noreply.github.com>
2022-10-28 14:30:32 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,9 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("s_show");
     document.querySelector('.burger_bg').classList.toggle('active')
     document.querySelector('.button_btn').classList.toggle('active')
+    document.querySelector("body").classList.toggle('active')
 }
-  
+
   function filterFunction() {
     var input, filter, ul, li, a, i;
     input = document.getElementById("myInput");
@@ -68,3 +69,73 @@ for(let i = 0; i < link.length; i++){
         content[i].classList.add('active');
     });
 }
+
+const wrap_btn = document.querySelector('.author_text_a')
+const wrap = document.querySelector('.buy_scene')
+const back = document.querySelector('.reg_back')
+const body = document.querySelector("body");
+
+wrap_btn.addEventListener('click' , function() {
+  wrap.style.display = 'flex'
+  body.classList.add('active')
+})
+
+back.addEventListener('click' , function(){
+  wrap.style.display = 'none'
+  body.classList.remove('active')
+})
+
+
+const circle = document.querySelectorAll('.dropdown-content a');
+
+for(let i = 0; i < circle.length; i++){
+    circle[i].addEventListener('click', function() {
+        body.classList.remove('active')
+        document.getElementById("myDropdown").classList.remove("s_show");
+        document.querySelector('.burger_bg').classList.remove('active')
+        document.querySelector('.button_btn').classList.remove('active')
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const left_btn = document.querySelector('.btn_left')
+// const right_btn = document.querySelector('.btn_right')
+// const img = document.querySelectorAll('.slider div')
+// let i = 0;
+
+// right_btn.addEventListener('click' ,  function() {
+//     img[i].classList.remove('active');
+//     i++;
+//     if(i == img.length){
+//         i=0;
+//     }
+//     img[i].classList.add('active');
+// })
+
+// left_btn.addEventListener('click' , function() {
+//     img[i].classList.remove('active');
+//     i--;
+//     if(i < 0){
+//         i = img.length - 1;
+//     }
+//     img[i].classList.add('active');
+// })
+
